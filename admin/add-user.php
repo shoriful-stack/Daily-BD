009f7e5caa21c078bc41545a63ae2138cc245966
Sa18-f <sisazid182713@gmail.com>
2024-09-15 18:50:39 +0600

    add-user file updated

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,67 +1,67 @@
-<?php 
-if(isset($_POST['save'])){
-include "config.php";
+<?php
+if (isset($_POST['save'])) {
+    include "config.php";
 
-$fname = mysqli_escape_string($conn, $_POST['fname']);
-$lname = mysqli_escape_string($conn, $_POST['lname']);
-$user = mysqli_escape_string($conn, $_POST['user']);
-$password = mysqli_escape_string($conn, md5($_POST['password']));
-$role = mysqli_escape_string($conn, $_POST['role']);
+    // Escape special characters in the input data
+    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+    $user = mysqli_real_escape_string($conn, $_POST['user']);
+    $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $role = mysqli_real_escape_string($conn, $_POST['role']);
+    $query = "SELECT username FROM user WHERE username = '{$user}'";
+    $result = mysqli_query($conn, $query);
 
-$query = "SELECT user FROM WHERE username = {$user}";
-$result = mysqli_query($conn, $query);
-
-if(mysqli_num_rows($result) > 0){
-    echo "User already exists";
-}
-else{
-    $query1 = "INSERT INTO user('first_name', 'last_name', 'username', 'password', 'role') VALUES('{$fname}', '{$lname}', '{$user}', '{$password}', '{$role}')";
-    if(mysqli_query($conn, $query1)){
-        header("Location : http://localhost:8080/Daily-Bd/admin/users.php");
+    if (mysqli_num_rows($result) > 0) {
+        echo "User already exists";
+    } else {
+        $query1 = "INSERT INTO user (first_name, last_name, username, password, role) 
+    VALUES ('{$fname}', '{$lname}', '{$user}', '{$password}', '{$role}')";
+        if (mysqli_query($conn, $query1)) {
+            header("Location : http://localhost:8080/Daily-Bd/admin/users.php");
+        }
     }
-}
 }
 ?>
 
 <?php include "header.php"; ?>
-  <div id="admin-content">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <h1 class="admin-heading">Add User</h1>
-              </div>
-              <div class="col-md-offset-3 col-md-6">
-                  <!-- Form Start -->
-                  <form  action="" method ="POST" autocomplete="off">
-                      <div class="form-group">
-                          <label>First Name</label>
-                          <input type="text" name="fname" class="form-control" placeholder="First Name" required>
-                      </div>
-                          <div class="form-group">
-                          <label>Last Name</label>
-                          <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
-                      </div>
-                      <div class="form-group">
-                          <label>User Name</label>
-                          <input type="text" name="user" class="form-control" placeholder="Username" required>
-                      </div>
+<div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="admin-heading">Add User</h1>
+            </div>
+            <div class="col-md-offset-3 col-md-6">
+                <!-- Form Start -->
+                <form action="" method="POST" autocomplete="off">
+                    <div class="form-group">
+                        <label>First Name</label>
+                        <input type="text" name="fname" class="form-control" placeholder="First Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Last Name</label>
+                        <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label>User Name</label>
+                        <input type="text" name="user" class="form-control" placeholder="Username" required>
+                    </div>
 
-                      <div class="form-group">
-                          <label>Password</label>
-                          <input type="password" name="password" class="form-control" placeholder="Password" required>
-                      </div>
-                      <div class="form-group">
-                          <label>User Role</label>
-                          <select class="form-control" name="role" >
-                              <option value="0">Normal User</option>
-                              <option value="1">Admin</option>
-                          </select>
-                      </div>
-                      <input type="submit"  name="save" class="btn btn-primary" value="Save" required />
-                  </form>
-                   <!-- Form End-->
-               </div>
-           </div>
-       </div>
-   </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <label>User Role</label>
+                        <select class="form-control" name="role">
+                            <option value="0">Normal User</option>
+                            <option value="1">Admin</option>
+                        </select>
+                    </div>
+                    <input type="submit" name="save" class="btn btn-primary" value="Save" required />
+                </form>
+                <!-- Form End-->
+            </div>
+        </div>
+    </div>
+</div>
 <?php include "footer.php"; ?>
